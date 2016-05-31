76777d9d2e9906670ea2ba0c32514b7d0ca15ccb
sravps7 <sravps7@gmail.com>
2016-05-31 16:26:09 +0530

    Added energy part

--- simulation.py
+++ simulation.py
@@ -4,20 +4,21 @@ import numpy as np
 #from matplotlib import style
 from sklearn.cluster import KMeans
 
-n_nodes=500
-n_targets=50
-r_sensing=50	
-alpha=0
-Eu=1
+n_nodes =500
+n_targets =50
+r_sensing =50	
+alpha = 0.25 
 E=1
-beta=0.25
+delta_E = 0.1*E
+beta=0.35	
 W=5
+
 zeroes_column=[0 for x in xrange(n_nodes)]
 
 target_mapping=[]
 Tu_initial=0
-Tu=[0for x in xrange(n_nodes)]
-#Tu=[]
+Tu=[0 for x in xrange(n_nodes)]
+
 TargetSU=[0 for x in xrange(n_nodes)]
 #TargetSU=[]
 targetval=n_targets
@@ -53,9 +54,9 @@ x_target = [x[0] for x in targets]
 y_target = [x[1] for x in targets]
 x_node = [x[0] for x in nodes]
 y_node = [x[1] for x in nodes]
-plt.plot(x_target, y_target,'r^',x_node, y_node,'bs')
+plt.plot(x_target, y_target,'g^',x_node, y_node,'bs')
 
-
+Eu=[E for x in xrange(n_nodes)]
 
 #Assuming that all the targets are being covered
 while targetval>0 :	#Waiting till all the targets have been covered
@@ -63,13 +64,14 @@ while targetval>0 :	#Waiting till all the targets have been covered
 		for j in range(0,n_targets):
 			TargetSU[i]=int(TargetSU[i])+int(connections[i][j])	
 		#Assuming the sensor energy does not change
-		Tu[i]=( (1-alpha*Eu/E ) - (beta*TargetSU[i]/n_targets) )*W - Tu_initial
+		Tu[i]=( (1-alpha*Eu[i]/E ) - (beta*TargetSU[i]/n_targets) )*W - Tu_initial
 	Tu_initial=min(Tu)
 	#print (Tu)
 	while (min(Tu)==Tu_initial):	#This is for multiple occurences of the same waiting time
 		sensor_index=Tu.index(Tu_initial)	#Index of the sensor whose waiting time got over
 		for k in range (0,n_targets):
 			if( connections[sensor_index][k] ==1):
+				Eu[sensor_index]=Eu[sensor_index]-delta_E
 				for p in range (0,n_nodes):
 					connections[p][k]=zeroes_column[p]#Removing that target from connections
 				targetval=targetval-1
@@ -78,12 +80,12 @@ while targetval>0 :	#Waiting till all the targets have been covered
 				y_sensor[k]=nodes[sensor_index][1]
 		Tu[sensor_index]=W
 
-#for i in range(0,n_targets):
-#	circle=plt.Circle((x_sensor[i],y_sensor[i]),r_sensing,color='r')
-#	fig = plt.gcf()
-#	fig.gca().add_artist(circle)
+for i in range(0,n_targets):
+	circle=plt.Circle((x_sensor[i],y_sensor[i]),r_sensing,color='r')
+	fig = plt.gcf()
+	fig.gca().add_artist(circle)
 
-plt.plot(x_sensor,y_sensor,'rs',x_target,y_target,'r^')
+plt.plot(x_sensor,y_sensor,'rs',x_target,y_target,'g^')
 #plt.plot(x_sensor,y_sensor,'rs')
 print "Red squares are sensors"
 print "Red triangles are targets"
@@ -99,7 +101,7 @@ labels = kmeans.labels_
 #K means algo used not according to the cost function mentioned in the paper. Maybe yes.
 
 	
-plt.scatter(centroids[:,0],centroids[:,1],marker ="x",s=150, linewidths=1, zorder=1)	
+#plt.scatter(centroids[:,0],centroids[:,1],marker ="x",s=150, linewidths=1, zorder=1)	
 node_relay=[False for x in xrange(n_nodes)]
 distance = [-1 for x in xrange(n_nodes)]
 sensor_centroid=[[0 for y in xrange(2)]for x in xrange(n_clusters)]
@@ -130,7 +132,7 @@ for m in range(0,n_clusters): # for a cluster
 				sensor_centroid[m][0]=(sensor_centroid[m][0]+nodes[temp_arr[q]][0])/len(set(temp_arr))
 				sensor_centroid[m][1]=(sensor_centroid[m][1]+nodes[temp_arr[q]][1])/len(set(temp_arr))
 
-plt.plot([row[0] for row in sensor_centroid], [row[1] for row in sensor_centroid] , 'b^')
+#plt.plot([row[0] for row in sensor_centroid], [row[1] for row in sensor_centroid] , 'b^')
 
 for j in range(len(target_mapping)) :	#For a sensor mapping target j
 	for i in range(0,n_nodes):	# For a node i
@@ -175,7 +177,7 @@ for i in range (n_clusters):
 #		x_relay.append(nodes[i][0])
 #		y_relay.append(nodes[i][1])
 	
-plt.plot(x_relay,y_relay,'g^')
+#plt.plot(x_relay,y_relay,'g^')
 print "Green triangles are relays"
 print "Number of targets are " + str(n_targets)
 print "Number of sensors are " + str(len(set(target_mapping)))
