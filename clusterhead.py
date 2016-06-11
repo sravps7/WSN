07fbc2f4c8826199ada91d92835cab8898b8d908
sravps7 <sravps7@gmail.com>
2016-06-11 18:53:16 +0530

    Added clusterhead selection code:

--- clusterhead.py
+++ clusterhead.py
@@ -97,13 +97,6 @@ kmeans.fit(nodes)
 centroids=kmeans.cluster_centers_
 labels = kmeans.labels_
 
-#K means algo used not according to the cost function mentioned in the paper. Maybe yes.
-
-	
-#plt.scatter(centroids[:,0],centroids[:,1],marker ="x",s=150, linewidths=1, zorder=1)	
-node_relay=[False for x in xrange(n_nodes)]
-distance = [-1 for x in xrange(n_nodes)]
-
 sum_x=[0 for x in xrange (n_clusters)]
 sum_y=[0 for x in xrange (n_clusters)]
 number=[0 for x in xrange(n_clusters)]
@@ -118,57 +111,62 @@ for i in range(n_clusters):
 	if (number[i] > 0):
 		sum_x[i]=sum_x[i]/number[i]
 		sum_y[i]=sum_y[i]/number[i]
+		
 
-
-#plt.plot([row[0] for row in sensor_centroid], [row[1] for row in sensor_centroid] , 'r^')
-
+p_alpha=1
+p_beta=0
+p_gamma=0
+p_delta=0
+ram=[0 for x in xrange(n_nodes)]
+memory=[0 for x in xrange(n_nodes)]
+distance=[0 for x in xrange(n_nodes)]
+power_node=[-1 for x in xrange(n_nodes)]
+#sum_x[i]  and sum_y[i] give the location of the centroid of cluster i.
+#oxygen_level, distance, Eu, ram, memory,
+#yet to read ram and memory from the txt file
+node_relay=[False for x in xrange(n_nodes)]
 for j in range(len(target_mapping)) :	#For a sensor mapping target j
 	for i in range(0,n_nodes):	# For a node i
 		if(  ( labels[target_mapping[j] ] == labels[i] ) and (target_mapping[j]!= i)) : #Checking if node i is in the same cluster as a sensor mapping target j # also making sure sensor is not the node
 			node_relay[i] = True
-			distance[i] = ( (x_node[i]-sum_x[labels[i]])**2 + ( y_node[i] - sum_y[ labels[i]])**2 )**0.5
-# Two arrays having distance from the nodes and whether the node is a potential sensor or not
+			
+for i in range(n_nodes):
+	distance[i] = ( ( nodes[i][0] - sum_x [ labels[i] ] )**2 + ( nodes[i][1] - sum_y [ labels[i] ] )**2 ) **0.5  
+	power_node[i] =  p_alpha*distance[i] + p_beta*Eu[i] + p_gamma*ram[i] + p_delta*memory[i]
+
+
 node_relay1=[False for x in xrange(n_nodes)]
 x_relay=[]
 y_relay=[]
 
-#remaining nodes
 temp_var=[0 for x in xrange(n_clusters)]
-min_dist=[0 for x in xrange(n_clusters)]
+min_power=[0 for x in xrange(n_clusters)]
 for i in range (0,n_clusters):	#iterating through all the clusters
 	for k in range(0,n_nodes):
 		if ( (labels[k]==i) and node_relay[k]):
 			temp_var[i]=k	#gives the first potential relay node whose label is i 
-			min_dist[i]=distance[ temp_var[i] ]	
+			min_power[i]=power_node[ temp_var[i] ]	
 			break		
 		else:
-			min_dist[i]=-1
+			min_power[i]=-1
 	 	#min_dist initialised as the first instance of distance of the label i
 	for j in range(n_nodes):	#iterating through all nodes  
 		if(node_relay[j] and labels[j]==i):	#If node j is a potential relay and belongs to the cluster i
-			if ( (distance[j] < min_dist[i]) and (min_dist[i] > 0) ):
-				min_dist[i]=distance[j]		
+			if ( (power_node[j] < min_power[i]) and (min_power[i] > 0) ):
+				min_power[i]=power_node[j]		
 				temp_var[i]=j
-
+				
 for i in range (n_clusters):				
-	if((min_dist[i]>=0)):
-		
+	if((min_power[i]>=0)):
 		x_relay.append(nodes[temp_var[i]][0])
 		y_relay.append(nodes[temp_var[i]][1])
-	#else:
-	#	node_relay[temp_var]=False		
 
 
-
-#for i in range (0,n_nodes):
-#	if(node_relay[i]==True):
-#		x_relay.append(nodes[i][0])
-#		y_relay.append(nodes[i][1])
-#plt.plot(x_target, y_target,'r^')
-plt.plot(x_node, y_node,'bs')
+plt.plot(x_node,y_node,'bs')
 plt.plot(x_relay,y_relay,'g^')
 plt.plot(x_sensor,y_sensor,'rs')
-plt.plot(sum_x, sum_y, 'r^')
+plt.plot(sum_x,sum_y,'gs')
+
 print "Blue squares are nodes"
 print "Red squares are sensors"
 print "Red triangles are targets"
@@ -178,10 +176,19 @@ print "Number of sensors are " + str(len(set(target_mapping)))
 print "Number of clusters are " + str(n_clusters)  
 print "Number of relays are " + str(len(x_relay))
 plt.show()
-	
-#Sufficient to make n_sensors number of clusters. This basically gives a set of sensors and relays with minimum distance from each other
-#Some clusters might arise which do not contain any nodes. is it safe to discard these clusters
-#cost = constant + distance to that node from the base station
-#minimise cost to get the required relay nodes	
 
-	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
